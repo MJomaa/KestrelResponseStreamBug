a1efef433fdb6b97dc7e65f4b7a777d76bf61061
Mahmut Jomaa <mahmut.jomaa@yahoo.de>
2015-09-05 11:40:25 +0200

    initial commit

---
+++ src/KestrelResponseStreamBug/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Microsoft.AspNet.Builder;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.DependencyInjection;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        private readonly IApplicationEnvironment _env;
+
+        public Startup(IApplicationEnvironment env)
+        {
+            _env = env;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (ctx) =>
+            {
+                var filePath = Path.Combine(_env.ApplicationBasePath, "Testfile");
+                using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024 * 64,
+                    FileOptions.Asynchronous | FileOptions.SequentialScan))
+                {
+                    await fileStream.CopyToAsync(ctx.Response.Body);
+                }
+            });
+        }
+    }
+}
